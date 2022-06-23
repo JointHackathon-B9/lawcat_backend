193c8b7d966cd346e9dd453aec4b8f86ee4fc25f
Gongmeda <gongmeda@gmail.com>
2022-06-23 15:19:19 +0900

    feat: verifyWebHook 처리 제거 + 채널 멤버 챗봇 유무 체킹 로직 추가

--- src/controllers/index.controller.ts
+++ src/controllers/index.controller.ts
@@ -22,19 +22,13 @@ class IndexController {
   // Stream Chat 서버에서 hook으로 요청을 보내는 API
   public hook = async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const valid = serverClient.verifyWebhook(JSON.stringify(req.body), req.header['x-signature']);
-
-      if (!valid) {
-        res.sendStatus(400);
-        return;
-      }
-
       const channelType = req.body.channel_type;
       const channelId = req.body.channel_id;
       const text = req.body.message.text;
       const userId = req.body.user.id;
+      const memberIds = req.body.members.map(m => m.user_id);
 
-      if (userId === CHATBOT_ID) {
+      if (userId === CHATBOT_ID || !memberIds.includes(CHATBOT_ID)) {
         res.sendStatus(200);
         return;
       }
@@ -43,10 +37,12 @@ class IndexController {
 
       // DB 조회
 
-      // 로캣 응답
+      const resultText = `AI processed message of '${text}'`;
+
+      // 챗봇 응답
       const channel = serverClient.channel(channelType, channelId);
       await channel.sendMessage({
-        text: `AI processed message of '${text}'`,
+        text: resultText,
         user_id: CHATBOT_ID,
       });
 
@@ -56,7 +52,7 @@ class IndexController {
     }
   };
 
-  // 챗봇 채널을 생성하는 API
+  // TODO: 챗봇 채널을 생성하는 API
   public createBotChannel = async (req: Request, res: Response, next: NextFunction) => {
     const botChannelName = '법률 상담 챗봇 - 로캣';
     const userId = req.body.userId;
@@ -71,7 +67,7 @@ class IndexController {
     }
   };
 
-  // 채널 삭제하는 API
+  // TODO: 채널 삭제하는 API
   public delteChannel = async (req: Request, res: Response, next: NextFunction) => {
     try {
     } catch (error) {
